cf4366d6a40f8c8e97bb19ea742f33562447663d
masudhossaincse <csemasud808@gmail.com>
2020-09-15 16:58:47 +0600

    working on quiz app

---
+++ script.js
@@ -0,0 +1,49 @@
+const quizData = [{
+        question: 'How old is he?',
+        a: '17',
+        b: '20',
+        c: '23',
+        d: '29',
+        correct: 'b'
+    },
+    {
+        question: 'What is the most used language in 2020?',
+        a: 'Java',
+        b: 'C',
+        c: 'PHP',
+        d: 'JavaScript',
+        correct: 'a'
+    },
+    {
+        question: 'How old is he?',
+        a: '17',
+        b: '20',
+        c: '23',
+        d: '29',
+        correct: 'b'
+    },
+    {
+        question: 'What is the most used language in 2020?',
+        a: 'Java',
+        b: 'C',
+        c: 'PHP',
+        d: 'JavaScript',
+        correct: 'a'
+    },
+    {
+        question: 'How old is he?',
+        a: '17',
+        b: '20',
+        c: '23',
+        d: '29',
+        correct: 'b'
+    },
+    {
+        question: 'What is the most used language in 2020?',
+        a: 'Java',
+        b: 'C',
+        c: 'PHP',
+        d: 'JavaScript',
+        correct: 'a'
+    },
+]
